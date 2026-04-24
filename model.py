df7e853f5942aa8178f008ef9d3924af740645f1
jenny-ngo-29 <jen.ngo1175@gmail.com>
2026-04-24 11:21:31 +0200

    confusion matrix code

--- model.py
+++ model.py
@@ -1,4 +1,5 @@
 import numpy as np
+import matplotlib as plt
 import tensorflow as tf
 from tensorflow.keras.applications import DenseNet121
 from tensorflow.keras.applications.densenet import preprocess_input
@@ -7,6 +8,7 @@ from tensorflow.keras.models import Model
 from tensorflow.keras.optimizers import Adam
 from tensorflow.keras.utils import to_categorical
 from tensorflow.keras.utils import image_dataset_from_directory
+from sklearn.metrics import confusion_matrix, ConfusionMatrixDisplay
 
 #load training and test data and preprocess
 train_ds = tf.keras.utils.image_dataset_from_directory(
@@ -66,6 +68,20 @@ history = model.fit(
     validation_data=test_ds,
     epochs=3
 )
+# Get predictions
+y_pred = model.predict(test_ds)
+y_pred_classes = np.argmax(y_pred, axis=1)
+
+# Get true labels
+y_true = np.concatenate([y for x, y in test_ds], axis=0)
+y_true_classes = np.argmax(y_true, axis=1)
+
+# Confusion matrix
+cm = confusion_matrix(y_true_classes, y_pred_classes)
+disp = ConfusionMatrixDisplay(confusion_matrix=cm, display_labels=class_names)
+disp.plot(cmap=plt.cm.Blues)
+plt.title('Confusion Matrix')
+plt.show()
 
 loss, acc = model.evaluate(test_ds)
 print("Accuracy:", acc)
